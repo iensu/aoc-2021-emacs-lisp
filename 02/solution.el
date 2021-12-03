30a986c2117c05f02c328b31acedad95c55f0f1d
Jens Östlund <jostlund@gmail.com>
2021-12-03 22:24:00 +0100

    Solved task 2 day 2

--- 02/solution.el
+++ 02/solution.el
@@ -24,3 +24,34 @@
                                   finally return (cl-reduce #'sum-offsets offsets))))
     (pcase-let ((`(,x . ,y) final-position))
       (* x y))))
+
+
+;; Task 2
+;;
+;; - down X increases your aim by X units.
+;; - up X decreases your aim by X units.
+;; - forward X does two things:
+;;   - It increases your horizontal position by X units.
+;;   - It increases your depth by your aim multiplied by X.
+(cl-flet ((parse-input (line)
+            (pcase-let* ((`(,direction ,value) (split-string line " ")))
+              `(,(intern direction) . ,(string-to-number value))))
+
+          (sum-offsets (a b)
+            (pcase-let* ((`(,x1 . ,y1)  a)
+                         (`(,x2 . ,y2) b))
+              `(,(+ x1 x2) . ,(+ y1 y2)))))
+
+  (let* ((input (aoc-utils/read-input "./input.txt"
+                                      #'parse-input))
+         (final-position (cl-loop with aim = 0
+                                  for (direction . value) being elements in input
+                                  if (eq direction 'forward)
+                                    collect `(,value . ,(* aim value)) into offsets
+                                  else
+                                    do (set 'aim (+ aim (if (eq direction 'down)
+                                                            value
+                                                          (* -1 value))))
+                                  finally return (cl-reduce #'sum-offsets offsets))))
+    (pcase-let ((`(,x . ,y) final-position))
+      (* x y))))
